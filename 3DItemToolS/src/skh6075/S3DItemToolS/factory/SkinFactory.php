b49b63519a1ab14240d74cacfc57a0c550b6e593
Song ki ho <44698603+GodVas@users.noreply.github.com>
2020-10-16 10:23:57 +0900

    Update SkinFactory.php

--- 3DItemToolS/src/skh6075/S3DItemToolS/factory/SkinFactory.php
+++ 3DItemToolS/src/skh6075/S3DItemToolS/factory/SkinFactory.php
@@ -12,9 +12,6 @@ use skh6075\S3DItemToolS\factory\skin\SkinReflection;
 use skh6075\S3DItemToolS\S3DItemToolS;
 
 class SkinFactory{
-
-    /** @var SkinFactory */
-    private static $instance = null;
     
     /** @var SkinReflection[] */
     private static $reflections = [];
@@ -26,30 +23,20 @@ class SkinFactory{
         "skins"
     ];
     
-    
-    public static function getInstance (): SkinFactory{
-        if (self::$instance === null) {
-            self::$instance = new self ();
-        }
-        return self::$instance;
-    }
-    
-    private function __construct () {
-    }
 
     public function init(): void{
-        $this->createData();
-        $this->loadReflections();
+        self::createData();
+        self::loadReflections();
     }
 
-    private function createData(): void{
+    private static function createData(): void{
         foreach (self::DIRECTORIES as $str) {
             if (!is_dir(S3DItemToolS::getInstance()->getDataFolder() . $str . DIRECTORY_SEPARATOR))
                 @mkdir(S3DItemToolS::getInstance()->getDataFolder() . $str . DIRECTORY_SEPARATOR);
         }
     }
 
-    private function loadReflections(): void{
+    private static function loadReflections(): void{
         foreach (array_diff(scandir(S3DItemToolS::getInstance()->getDataFolder() . "models" . DIRECTORY_SEPARATOR), [ '.', '..' ]) as $value) {
             if (!isset(explode('.', $value) [1]) || explode('.', $value) [1] !== TYPE_JSON) {
                 continue;
