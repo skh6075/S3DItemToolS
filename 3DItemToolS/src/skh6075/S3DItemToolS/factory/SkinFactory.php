4709b9468e7a80758c170ac418f419f36725766e
Song ki ho <44698603+GodVas@users.noreply.github.com>
2020-10-16 10:40:59 +0900

    Singlethon
    Singlethon

--- 3DItemToolS/src/skh6075/S3DItemToolS/factory/SkinFactory.php
+++ 3DItemToolS/src/skh6075/S3DItemToolS/factory/SkinFactory.php
@@ -3,15 +3,13 @@
 
 namespace skh6075\S3DItemToolS\factory;
 
-/**
- * @return string
- */
-define ("TYPE_JSON", "json");
-
 use skh6075\S3DItemToolS\factory\skin\SkinReflection;
 use skh6075\S3DItemToolS\S3DItemToolS;
 
 class SkinFactory{
+    
+    /** @var SkinFactory */
+    private static $instance = null;
     
     /** @var SkinReflection[] */
     private static $reflections = [];
@@ -24,19 +22,29 @@ class SkinFactory{
     ];
     
 
+    public static function getInstance (): SkinFactory{
+        if (self::$instance === null) {
+            self::$instance = $this;
+        }
+        return self::$instance;
+    }
+    
+    private function __construct () {
+    }
+    
     public function init(): void{
-        self::createData();
-        self::loadReflections();
+        $this->createData();
+        $this->loadReflections();
     }
 
-    private static function createData(): void{
+    private function createData(): void{
         foreach (self::DIRECTORIES as $str) {
             if (!is_dir(S3DItemToolS::getInstance()->getDataFolder() . $str . DIRECTORY_SEPARATOR))
                 @mkdir(S3DItemToolS::getInstance()->getDataFolder() . $str . DIRECTORY_SEPARATOR);
         }
     }
 
-    private static function loadReflections(): void{
+    private function loadReflections(): void{
         foreach (array_diff(scandir(S3DItemToolS::getInstance()->getDataFolder() . "models" . DIRECTORY_SEPARATOR), [ '.', '..' ]) as $value) {
             if (!isset(explode('.', $value) [1]) || explode('.', $value) [1] !== TYPE_JSON) {
                 continue;
